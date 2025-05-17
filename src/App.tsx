3dc1a26938a04906042790d52875907a82256035
chanakya29092004 <kasachanakya2022@gmail.com>
2025-05-17 12:45:04 +0530

    error for ai

--- src/App.tsx
+++ src/App.tsx
@@ -26,11 +26,8 @@ function App() {
       <AuthProvider>
         <ToastProvider>
           <Routes>
-            {/* Redirect root to landing */}
-            <Route path="/" element={<Landing />} />
-            <Route path="/landing" element={<Landing />} />
-
             {/* Public routes */}
+            <Route path="/landing" element={<Landing />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
 
